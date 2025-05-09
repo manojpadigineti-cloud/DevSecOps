39eda4a6ac0893462f78e59743c4224e9ddcdf9b
Manoj <padigineti.manojkumar@gmail.com>
2025-05-09 09:35:36 +0530

    added terraform spot instance block

--- terraform/AWS/roboshop_project/modules/ec2-spot/main.tf
+++ terraform/AWS/roboshop_project/modules/ec2-spot/main.tf
@@ -4,6 +4,7 @@ resource "aws_instance" "ec2" {
   subnet_id = var.ec2_subnet
   vpc_security_group_ids = [var.security_group]
   instance_market_options {
+    market_type = "spot"
     spot_options {
       instance_interruption_behavior = "stop"
       spot_instance_type = "persistent"
