a5844cf51fff5893940c0e3c2f34040909ee2c2e
Manoj <padigineti.manojkumar@gmail.com>
2025-05-09 22:59:55 +0530

    Ansible github_runner configuration for github actions

--- terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
+++ terraform/AWS/roboshop_project/modules/terraform_provisioner/main.tf
@@ -9,10 +9,10 @@ resource "null_resource" "terraform_provisioner" {
 
   provisioner "remote-exec" {
     inline = [
-        "git clone https://github.com/ManojPadigineti/ansible_playbook.git",
-        "cd /home/ec2-user/ansible_playbook ; git pull ; sudo bash /home/ec2-user/ansible_playbook/Install_terraform.sh",
-        "sudo bash /home/ec2-user/ansible_playbook/install_ansible.sh",
-        "cd /home/ec2-user/ansible_playbook/ ; ansible-playbook -i localhost playbook.yml -e var_file=hashicorp"
+        "git clone https://github.com/manojpadigineti-cloud/DevSecOps.git",
+        "git pull ; sudo bash /home/ec2-user/DevSecOps/ansible/Install_terraform.sh",
+        "sudo bash /home/ec2-user/DevSecOps/ansible/install_ansible.sh",
+        "cd /home/ec2-user/DevSecOps/ansible ; ansible-playbook -i localhost playbook.yml -e var_file=hashicorp"
     ]
   }
 }
