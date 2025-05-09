a61e69f219d67e206b496c132c7fb23294abe314
Manoj <padigineti.manojkumar@gmail.com>
2025-05-09 09:25:27 +0530

    added terraform spot instance block

--- terraform/AWS/roboshop_project/network/outputs.tf
+++ terraform/AWS/roboshop_project/network/outputs.tf
@@ -1,7 +1,13 @@
+# for <key_var>, <value_var> in <map> : <key_var> => <value_var>.<output>
+
 output "security_group_id" {
-  value = module.security_group.sg_id
+  value = {
+    for sg, value in module.security_group : sg => value.sg_id
+  }
 }
 
-output "subnet_id" {
-  value = module.subnets.subnet_id
+output "subnets_id" {
+  value = {
+    for subnet, all_subnet_id in module.subnets : subnet => all_subnet_id.subnet_id
+  }
 }
