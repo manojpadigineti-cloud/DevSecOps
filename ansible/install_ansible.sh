54aa3033f5d3e422bd3d0fbf4359dfb08ea6686c
Manoj <padigineti.manojkumar@gmail.com>
2025-05-09 17:00:36 +0530

    added terraform spot instance block

--- ansible/install_ansible.sh
+++ ansible/install_ansible.sh
@@ -1,3 +1,3 @@
 sudo yum update -y
-sudo yum install epel-release
-sudo yum install ansible
+sudo yum install epel-release -y
+sudo yum install ansible -y
---
+++ github_cli/install_gh_cli.sh
@@ -0,0 +1,6 @@
+sudo dnf config-manager --add-repo https://cli.github.com/packages/rpm/gh-cli.repo
+sudo dnf install gh
+gh auth refresh -h github.com -s admin:org # To login organisation level
+
+# Command to get the token
+gh api   -X POST   orgs/manojpadigineti-cloud/actions/runners/registration-token   -H "Accept: application/vnd.github+json"   --jq .token
