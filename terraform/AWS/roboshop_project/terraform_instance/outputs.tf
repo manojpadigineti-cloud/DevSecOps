7b7c314f083e853a3b334f85e79d65e70d2c8a69
Manoj <padigineti.manojkumar@gmail.com>
2025-05-15 23:52:29 +0530

    Terraform_Vault secrets module created

---
+++ terraform/AWS/roboshop_project/terraform_instance/outputs.tf
@@ -0,0 +1,3 @@
+output "output_after_provision_of_terraform_Vault_instance" {
+  value = "Provision hashicor_vault_secrets_module and provide vault_token using -var, Before provisioning roboshop instances add github_token Manually"
+}
