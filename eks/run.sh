333f3c7523d1c96374eef244634c93bd00c9b2d7
Manoj <padigineti.manojkumar@gmail.com>
2025-10-11 15:19:59 +0530

    Helm k8s implementation

--- eks/run.sh
+++ eks/run.sh
@@ -1,3 +1,5 @@
 for app in cart catalogue frontend payment shipping user; do
-  cd /home/ec2-user/DevSecOps/eks && git pull && helm upgrade -i "$(app)-deployment" . -f env-dev/$(app).yaml;
+  cd /home/ec2-user/DevSecOps/eks
+  git pull
+  helm upgrade -i ${app}-deployment . -f env-dev/${app}.yaml
 done
