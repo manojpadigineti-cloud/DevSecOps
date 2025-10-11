8a58639278573071d93e20ce0882f71c2ed4cbd4
Manoj <padigineti.manojkumar@gmail.com>
2025-10-11 16:07:49 +0530

    Helm k8s implementation

--- eks/delete.sh
+++ eks/delete.sh
@@ -1,5 +1,5 @@
 for app in cart catalogue frontend payment shipping user; do
   cd /home/ec2-user/DevSecOps/eks
   git pull
-  helm uninstall -i ${app}-deployment . -f env-dev/${app}.yaml
+  helm uninstall ${app}-deployment . -f env-dev/${app}.yaml
 done
